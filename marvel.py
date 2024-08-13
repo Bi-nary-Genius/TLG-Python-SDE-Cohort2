933cee641303086b10aa886ba12efe8663ae14dd
Sherie Chandler <sherie.chandler@gmail.com>
2024-08-13 15:59:53 +0000

    mixed lists

---
+++ marvel.py
@@ -0,0 +1,9 @@
+marvelCharacters = ["Spider-Man", "Black Panther", "Thor"]
+
+marvelCharacters.append("Doctor Strange")
+
+favoriteCharacter = marvelCharacters[-1]
+
+marvelCharacters.append("Doctors Strange")
+
+print(f"{favoriteCharacter} is my favorite character! ")
