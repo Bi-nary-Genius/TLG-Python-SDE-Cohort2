8ff3566081d51a0321e0eba0744432f3b8ced2e9
Sherie Chandler <sherie.chandler@gmail.com>
2024-08-13 17:08:40 +0000

    learning about dictionaries

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+"""
+
+Dictionaries are the most common data type in large quantities in python.
+
+Following skills are required:
+    pull information out of dictionaries
+    change (add to/remove keys/values)
+
+    """
+
+    got = {
+            "name": "Jon Snow",
+            "gender": "Male",
+            "culture": "Northern",
+            "born": "In 283 AC",
+
+            }
+    # a list of all the keys in this dictionary
+    print("What would you like to know about Jon Snow? Pick from the following: ")
+
+    # remove a key/value pair
