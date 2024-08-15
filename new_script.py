df91de2c64332a07ab46232dad9e7d6bd8f1df16
Sherie Chandler <sherie.chandler@gmail.com>
2024-08-15 13:13:42 +0000

    Add new_script.py

---
+++ new_script.py
@@ -0,0 +1,37 @@
+#Part 1
+with open("dracula.txt","r") as foo:
+
+#Part 2
+for line in foo:
+    print(line)
+
+#Part 3
+for line in foo:
+    if "vampire" in line:
+        print(line)
+
+#Part 4
+for line in foo:
+    if "vampire" in line.lower():
+        print(line)
+
+#Part 5
+count = 0
+for line in foo:
+    if "vampire" in line.lower():
+        count +=1
+        print(count)
+
+#Part 6
+counter= 0
+
+with open("dracula.txt","r") as foo:
+    with open("vampytimex.txt","w") as fang:
+        for line in foo:
+            if "vampire" in line.lower():
+                print(line)
+                counter += 1
+                fang.write(line)
+
+print(counter)
+
